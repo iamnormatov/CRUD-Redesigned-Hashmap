af67d271000a5b83921c3d94349f56414f7548f4
New-Bucket <azizjonnormatov437@gmail.com>
2023-03-24 11:46:09 +0500

    crud

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,7 @@
 public class Card {
     private Integer cardId;
     private String cardName;
-    private Long CardNumber;
+    private Long CardNumber; 
     private Integer cardCode;
 
     public Card(String cardName, Long cardNumber, Integer cardCode) {
